eec34796e05e0277c23928e589e5ed12ebe76aa4
Daniel Kipshagen <daniel@kipshagen.com>
2024-12-10 20:36:07 +0100

    solve puzzle 2 for day 10

---
+++ day10/day10-2.zig
@@ -0,0 +1,98 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const input = @embedFile("input.txt");
+
+    std.debug.print("\nCalculating all trails to peaks...", .{});
+    const map = try parseInput(allocator, input);
+    const peaks = try calcTrailheadScores(map);
+    std.debug.print("\npeaks: {d}", .{peaks});
+}
+
+fn parseInput(allocator: std.mem.Allocator, input: []const u8) ![][]u8 {
+    var map = std.ArrayList([]u8).init(allocator);
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        var row = std.ArrayList(u8).init(allocator);
+        for (line) |ch| {
+            const height: u8 = ch - '0';
+            try row.append(height);
+        }
+        try map.append(try row.toOwnedSlice());
+    }
+    return try map.toOwnedSlice();
+}
+
+const Position = struct {
+    x: i32,
+    y: i32,
+};
+
+fn calcTrailheadScores(map: [][]u8) !usize {
+    var score: usize = 0;
+    // for all trailheads
+    for (map, 0..) |row, y| {
+        for (row, 0..) |height, x| {
+            if (height == 0) {
+                // trailhead
+                score += try findTrails(map, .{ .x = @intCast(x), .y = @intCast(y) });
+            }
+        }
+    }
+    return score;
+}
+
+test calcTrailheadScores {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const example =
+        \\89010123
+        \\78121874
+        \\87430965
+        \\96549874
+        \\45678903
+        \\32019012
+        \\01329801
+        \\10456732
+    ;
+
+    const map = try parseInput(allocator, example);
+    try expectEqual(81, try calcTrailheadScores(map));
+}
+
+fn findTrails(map: [][]u8, start: Position) !usize {
+    var score: usize = 0;
+    const height = map[@intCast(start.y)][@intCast(start.x)];
+    // std.debug.print("\n{d},{d}: {d}", .{ start.x, start.y, height });
+    // check all directions
+    const directions: [4]Position = .{ .{ .x = 0, .y = 1 }, .{ .x = 1, .y = 0 }, .{ .x = -1, .y = 0 }, .{ .x = 0, .y = -1 } };
+    for (directions) |direction| {
+        const new_position = .{
+            .x = start.x + direction.x,
+            .y = start.y + direction.y,
+        };
+        // continue with next position if not a valid position
+        if (new_position.x < 0 or new_position.y < 0 or new_position.x >= map[0].len or new_position.y >= map.len)
+            continue;
+
+        const new_height = map[@intCast(new_position.y)][@intCast(new_position.x)];
+        // if one higher, found a trail
+        if (new_height > height and new_height - height == 1) {
+            if (new_height == 9) {
+                // found peak
+                score += 1;
+                // std.debug.print("\npeak: {d},{d}: {d}", .{ new_position.x, new_position.y, new_height });
+            } else {
+                // recurse
+                score += try findTrails(map, new_position);
+            }
+        }
+    }
+    return score;
+}
