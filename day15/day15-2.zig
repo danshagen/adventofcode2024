d4745b981979b6c91d10657545ee7f411843e42d
Daniel Kipshagen <daniel@kipshagen.com>
2024-12-15 16:22:02 +0100

    solve puzzle 2 for day 15

--- day15/day15-2.zig
+++ day15/day15-2.zig
@@ -23,37 +23,40 @@ fn parseInput(allocator: std.mem.Allocator, input: []const u8) !struct { map: []
     var map = std.ArrayList([]u8).init(allocator);
     var rows = std.mem.tokenizeScalar(u8, first_part, '\n');
     while (rows.next()) |row| {
-        const new_row = try allocator.alloc(u8, row.len);
-        @memcpy(new_row, row);
-        try map.append(new_row);
+        var list = std.ArrayList(u8).init(allocator);
+        for (row) |ch| {
+            if (ch == '#') {
+                try list.append('#');
+                try list.append('#');
+            }
+            if (ch == 'O') {
+                try list.append('[');
+                try list.append(']');
+            }
+            if (ch == '.') {
+                try list.append('.');
+                try list.append('.');
+            }
+            if (ch == '@') {
+                try list.append('@');
+                try list.append('.');
+            }
+        }
+        try map.append(try list.toOwnedSlice());
     }
 
     // parse movements
     var movements = std.ArrayList(u8).init(allocator);
     for (second_part) |ch| {
-        if (ch == '#') {
-            try movements.append('#');
-            try movements.append('#');
-        }
-        if (ch == 'O') {
-            try movements.append('[');
-            try movements.append(']');
-        }
-        if (ch == '.') {
-            try movements.append('.');
-            try movements.append('.');
-        }
-        if (ch == '@') {
-            try movements.append('@');
-            try movements.append('.');
-        }
+        if (ch != '\n')
+            try movements.append(ch);
     }
 
     return .{ .map = try map.toOwnedSlice(), .movements = try movements.toOwnedSlice() };
 }
 
 // move a block: if possible move and return true, else return false
-fn move(map: [][]u8, x: usize, y: usize, direction: u8) bool {
+fn move(map: [][]u8, x: usize, y: usize, direction: u8, peek: bool) bool {
     const block = map[y][x];
     const next_x = switch (direction) {
         '^' => x,
@@ -75,32 +78,62 @@ fn move(map: [][]u8, x: usize, y: usize, direction: u8) bool {
             break :el 0;
         },
     };
+
     const next = map[next_y][next_x];
-
-    if (next == '#')
-        return false;
-
-    if (next == '.') {
-        // move
-        map[next_y][next_x] = block;
-        map[y][x] = '.';
-        return true;
-    }
-
-    if (next == 'O') {
-        // try to move block
-        if (move(map, next_x, next_y, direction)) {
-            // move
-            map[next_y][next_x] = block;
-            map[y][x] = '.';
-            return true;
-        } else {
-            return false;
+    var neighbour: ?u8 = null;
+    var neighbour_next: u8 = '.'; // neighbour_next is the next if we also check the second part of a block
+    var neighbour_x: usize = 0;
+    if (direction == '^' or direction == 'v') {
+        if (block == '[') {
+            neighbour = ']';
+            neighbour_x = next_x + 1;
+            neighbour_next = map[next_y][neighbour_x];
+        } else if (block == ']') {
+            neighbour = '[';
+            neighbour_x = next_x - 1;
+            neighbour_next = map[next_y][neighbour_x];
         }
     }
 
-    std.debug.assert(false);
-    return false;
+    // if the next (or the block neighbour next, if there) is a wall, cannot move
+    if (next == '#' or neighbour_next == '#')
+        return false;
+
+    // if the next (or the block neighbour next, if there) is a block, try to move
+    if (next == '[' or next == ']') {
+        // try to move block
+        if (!move(map, next_x, next_y, direction, true)) {
+            // could not move next (peeking)
+            return false;
+        }
+        // actually do the move of next
+        if (!peek and next != block)
+            _ = move(map, next_x, next_y, direction, false);
+    }
+
+    if (neighbour_next == '[' or neighbour_next == ']') {
+        // check if block neighbour next can also be moved
+        if (!move(map, neighbour_x, next_y, direction, true)) {
+            // could not move next of block neighbour (peeking)
+            return false;
+        }
+        // actually do the move of neighbour_next
+        if (!peek) {
+            _ = move(map, neighbour_x, next_y, direction, false);
+        }
+    }
+
+    if (!peek) {
+        // move
+        map[next_y][next_x] = block;
+        map[y][x] = '.';
+        if (neighbour != null) {
+            // move the neighbour block
+            map[next_y][neighbour_x] = map[y][neighbour_x];
+            map[y][neighbour_x] = '.';
+        }
+    }
+    return true;
 }
 
 fn calcBoxCoordinateTotal(map: [][]u8, movements: []u8) usize {
@@ -123,7 +156,7 @@ fn calcBoxCoordinateTotal(map: [][]u8, movements: []u8) usize {
     }
 
     for (movements, 1..) |direction, n| {
-        if (move(map, x, y, direction)) {
+        if (move(map, x, y, direction, false)) {
             // new positions
             switch (direction) {
                 '^' => {
@@ -146,17 +179,17 @@ fn calcBoxCoordinateTotal(map: [][]u8, movements: []u8) usize {
 
         // print map
         _ = n;
-        std.debug.print("\n\nstep {d} {c}\n", .{ n, direction });
-        for (map) |row| {
-            std.debug.print("{s}\n", .{row});
-        }
+        // std.debug.print("\n\nstep {d} {c}\n", .{ n, direction });
+        // for (map) |row| {
+        //     std.debug.print("{s}\n", .{row});
+        // }
     }
 
     // calculate GPS total
     var total: usize = 0;
     for (map, 0..) |row, _y| {
         for (row, 0..) |ch, _x| {
-            if (ch == 'O') {
+            if (ch == '[') {
                 total += 100 * _y + _x;
             }
         }
@@ -176,5 +209,5 @@ test parseInput {
     const parsed = try parseInput(allocator, example);
 
     // ADD RESULT FROM EXAMPLE
-    try expectEqual(0, calcBoxCoordinateTotal(parsed.map, parsed.movements));
+    try expectEqual(”, calcBoxCoordinateTotal(parsed.map, parsed.movements));
 }
